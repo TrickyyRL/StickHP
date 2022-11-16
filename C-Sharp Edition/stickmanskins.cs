9784d01f656b76354fcf03c5041480d8073ea872
Tricky6969 <115975534+Tricky6969@users.noreply.github.com>
2022-11-16 11:57:21 +0400

    Fixed the / \ unexpected escape problem
    Had to replace / \  to  |  |

--- C-Sharp Edition/stickmanskins.cs
+++ C-Sharp Edition/stickmanskins.cs
@@ -1,221 +1,215 @@
-namespace Namespace {
+namespace StickPeople {
     
-    public static class Module {
+    public static class StickCharacters {
         
         public static object StickMan1() {
             Console.WriteLine(" ! ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan2() {
             Console.WriteLine(" @ ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan3() {
             Console.WriteLine(" # ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan4() {
             Console.WriteLine(" $ ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan5() {
             Console.WriteLine(" % ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan6() {
             Console.WriteLine(" ^ ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan7() {
             Console.WriteLine(" & ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan8() {
             Console.WriteLine(" * ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan9() {
             Console.WriteLine(" - ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan10() {
             Console.WriteLine(" = ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan11() {
             Console.WriteLine(" + ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan12() {
             Console.WriteLine(" o ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan13() {
             Console.WriteLine(" 0 ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan14() {
             Console.WriteLine(" 8 ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan15() {
             Console.WriteLine(" m ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan16() {
             Console.WriteLine(" w ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan17() {
             Console.WriteLine(" x ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan18() {
             Console.WriteLine(" s ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan19() {
             Console.WriteLine(" g ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan20() {
             Console.WriteLine(" h ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan21() {
             Console.WriteLine(" v ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan22() {
             Console.WriteLine(" n ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan23() {
             Console.WriteLine(" ? ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan24() {
             Console.WriteLine(" '' ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan25() {
             Console.WriteLine(" : ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan26() {
             Console.WriteLine(" ; ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan27() {
             Console.WriteLine(" ~ ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan28() {
             Console.WriteLine(" ` ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan29() {
             Console.WriteLine(" [ ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan30() {
             Console.WriteLine(" ] ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan31() {
             Console.WriteLine(" { ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan32() {
             Console.WriteLine(" } ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan33() {
             Console.WriteLine(" > ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan34() {
             Console.WriteLine(" < ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
         
         public static object StickMan35() {
             Console.WriteLine(" / ");
             Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
-        }
-        
-        public static object StickMan36() {
-            Console.WriteLine(" \ ");
-            Console.WriteLine("-|-");
-            Console.WriteLine("/ \ ");
+            Console.WriteLine("| | ");
         }
     }
 }
