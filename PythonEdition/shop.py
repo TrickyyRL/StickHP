3b4e775789bb8b1105a7847c6569f8c69258ec1e
Tricky6969 <115975534+Tricky6969@users.noreply.github.com>
2022-11-17 13:41:47 +0400

    Added prices to each skin

--- PythonEdition/shop.py
+++ PythonEdition/shop.py
@@ -1,206 +1,240 @@
 from stickmanskins import *
 import os
+import random
 player1money = 10
 player2money = 10
+stickman1price=10
+stickman2price=30
+stickman3price=40
+stickman4price=1000
+stickman5price=100
+stickman6price=20
+stickman7price=10
+stickman8price=15
+stickman9price=25
+stickman10price=50
+stickman11price=51
+stickman12price=10
+stickman13price=100
+stickman14price=200
+stickman15price=20
+stickman16price=30
+stickman17price=182
+stickman18price=289
+stickman19price=10
+stickman20price=75
+stickman21price=23
+stickman22price=459
+stickman23price=random.randint(1,699)
+stickman24price=100
+stickman25price=222
+stickman26price=111
+stickman27price=600
+stickman28price=120
+stickman29price=5000
+stickman30price=5000
+stickman31price=2500
+stickman32price=2500
+stickman33price=1250
+stickman34price=1250
+stickman35price=random.randint(1,200)
+
 def clear():
   os.system("clear")
 def ShopHasOpened():
+  clear()
   print(StickMan1())
   while True:
-    next=int(input("Enter a number to see what skin is avaliable: "))
+    next=int(input("Enter a number to see what skin is avaliable, there are 35 skins to choose from: "))
     if next==1:
       clear()
       print(StickMan1())
-      print(next,"/ 36")
+      print("Price:",stickman1price,"coins")
+      print(next,"/ 35")
     if next==2:
       clear()
       print(StickMan2())
-      print(next,"/ 36")
+      print(next,"/ 35")
        
     if next==3:
       clear()
       print(StickMan3())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==4:
       clear()
       print(StickMan4())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==5:
       clear()
       print(StickMan5())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==6:
       clear()
       print(StickMan6())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==7:
       clear()
       print(StickMan7())
-      print(next,"/ 36")
+      print(next,"/ 35")
     
     if next==8:
       clear()
       print(StickMan8())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==9:
       clear()
       print(StickMan9())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
   
     if next==10:
       clear()
       print(StickMan10())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
    
     if next==11:
       clear()
       print(StickMan11())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==12:
       clear()
       print(StickMan12())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==13:
       clear()
       print(StickMan13())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==14:
       clear()
       print(StickMan14())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==15:
       clear()
       print(StickMan15())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==16:
       clear()
       print(StickMan16())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==17:
       clear()
       print(StickMan17())
-      print(next,"/ 36")
+      print(next,"/ 35")
      
       
     if next==18:
       clear()
       print(StickMan18())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==19:
       clear()
       print(StickMan19())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
   
     if next==20:
       clear()
       print(StickMan20())
-      print(next,"/ 36")
+      print(next,"/ 35")
      
       
     if next==21:
       clear()
       print(StickMan21())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==22:
       clear()
       print(StickMan22())
-      print(next,"/ 36")
+      print(next,"/ 35")
      
       
     if next==23:
       clear()
       print(StickMan23())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==24:
       clear()
       print(StickMan24())
-      print(next,"/ 36")
+      print(next,"/ 35")
      
       
     if next==25:
       clear()
       print(StickMan25())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==26:
       clear()
       print(StickMan26())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
       
     if next==27:
       clear()
       print(StickMan27())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==28:
       clear()
       print(StickMan28())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==29:
       clear()
       print(StickMan29())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==30:
       clear()
       print(StickMan30())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==31:
       clear()
       print(StickMan31())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==32:
       clear()
       print(StickMan32())
-      print(next,"/ 36")
+      print(next,"/ 35")
   
     if next==33:
       clear()
       print(StickMan33())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==34:
       clear()
       print(StickMan34())
-      print(next,"/ 36")
+      print(next,"/ 35")
       
     if next==35:
       clear()
       print(StickMan35())
-      print(next,"/ 36")
-      
-    if next==36:
-      clear()
-      print(StickMan36())
-      print(next,"/ 36")
+      print(next,"/ 35")
