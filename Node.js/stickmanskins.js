0e697e35fedfe9727fce21d42a684509ef917e8a
Tricky6969 <115975534+Tricky6969@users.noreply.github.com>
2022-11-21 10:19:32 +0400

    Update and rename stickmanskins.py to stickmanskins.js

---
+++ Node.js/stickmanskins.js
@@ -0,0 +1,209 @@
+function StickMan1() {
+  console.log(" ! ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan2() {
+  console.log(" @ ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan3() {
+  console.log(" # ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan4() {
+  console.log(" $ ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan5() {
+  console.log(" % ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan6() {
+  console.log(" ^ ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan7() {
+  console.log(" & ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan8() {
+  console.log(" * ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan9() {
+  console.log(" - ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan10() {
+  console.log(" = ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan11() {
+  console.log(" + ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan12() {
+  console.log(" o ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan13() {
+  console.log(" 0 ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan14() {
+  console.log(" 8 ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan15() {
+  console.log(" m ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan16() {
+  console.log(" w ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan17() {
+  console.log(" x ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan18() {
+  console.log(" s ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan19() {
+  console.log(" g ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan20() {
+  console.log(" h ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan21() {
+  console.log(" v ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan22() {
+  console.log(" n ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan23() {
+  console.log(" ? ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan24() {
+  console.log(" ' ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan25() {
+  console.log(" : ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan26() {
+  console.log(" ; ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan27() {
+  console.log(" ~ ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan28() {
+  console.log(" ` ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan29() {
+  console.log(" [ ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan30() {
+  console.log(" ] ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan31() {
+  console.log(" { ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan32() {
+  console.log(" } ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan33() {
+  console.log(" > ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan34() {
+  console.log(" < ");
+  console.log("-|-");
+  console.log("| | ");
+}
+
+function StickMan35() {
+  console.log(" / ");
+  console.log("-|-");
+  console.log("| | ");
+}
