467ac9b13c333912c37f314cf3f885c472cc9ab4
Tricky6969 <115975534+Tricky6969@users.noreply.github.com>
2022-11-29 06:55:31 +0400

    figured out how to add a stickman class

--- JavaEdition/stickmanskins.java
+++ JavaEdition/stickmanskins.java
@@ -1,1 +1,11 @@
+public class stickmanskins {
+  static void StickMan1() {
+    System.out.println(" ! ");
+    System.out.println("-|-");
+    System.out.println("| |");
+  }
 
+  public static void main(String[] args) {
+    
+  }
+}
